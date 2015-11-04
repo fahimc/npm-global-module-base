6f1e3ce247cf88c4ef93167522a38173dffc8aca
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2015-11-04 21:53:18 +0000

    added author support

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function (grunt) {
   function getName() {
     var name = grunt.option('name');
     var repo = grunt.option('repo');
-    console.log(repo);
+    var author = grunt.option('author');
     if (!name) {
       grunt.fail.fatal('Name not specified. please type --name=ModuleName');
     }else{
@@ -15,6 +15,9 @@ module.exports = function (grunt) {
         content.repository.url = repo;
         content.bugs.url = repo.replace('.git','/issues');
         content.homepage = repo.replace('.git','');
+      }
+      if(author){
+        content.author = author;
       }
       grunt.file.write('package.json',JSON.stringify(content,null,2));
     }
